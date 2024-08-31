b326889f549c13690d066da5eac8190310e10dec
zeke <osafoezekiel@gmail.com>
2024-08-31 08:03:32 +0100

    Hero done

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@
 // import { Button } from './components/ui/button'
 
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Header />
 
       {/* Hero */}
+      <Hero />
     </>
   )
 }
---
+++ src/shared/Data.jsx
@@ -0,0 +1,25 @@
+const CarMakes = [
+  {
+    id: 1,
+    name: 'Audi',
+  },
+  {
+    id: 2,
+    name: 'BMW',
+  },
+  {
+    id: 3,
+    name: 'Ford',
+  },
+]
+
+const Pricing = [
+  { id: 1, amount: '1000$' },
+  { id: 2, amount: '7000$' },
+  { id: 3, amount: '10000$' },
+]
+
+export default {
+  CarMakes,
+  Pricing,
+}
