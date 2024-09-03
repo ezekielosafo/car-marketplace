2628ba6dfff5b4803a74aa0322d40d58d1b8e72f
zeke <osafoezekiel@gmail.com>
2024-09-03 07:49:35 +0100

    ...

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -1,32 +1,38 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function CarItem({ car }) {
   return (
     <div>
-      <img src={car?.image} width={300} height={250} />
-      <div></div>
+      <img src={car?.image} width={300} height={250} alt={car?.name} />
+      <div className="p-4">
+        <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
+      </div>
     </div>
   )
+}
+
+CarItem.propTypes = {
+  car: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default CarItem
 
 // import React from 'react'
-// import PropTypes from 'prop-types'
 
 // function CarItem({ car }) {
 //   return (
 //     <div>
-//       <img src={car.image} width={300} height={250} alt="Car" />
+//       <img src={car?.image} width={300} height={250} />
+//       <div className="p-4">
+//         <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
+//       </div>
 //     </div>
 //   )
-// }
-
-// CarItem.propTypes = {
-//   car: PropTypes.shape({
-//     image: PropTypes.string.isRequired,
-//   }).isRequired,
 // }
 
 // export default CarItem
--- src/components/MostSearchedCar.jsx
+++ src/components/MostSearchedCar.jsx
@@ -10,7 +10,6 @@ function MostSearchedCar() {
       <h2 className="font-bold text-3xl text-center my-16">
         Most Searched Cars
       </h2>
-      {/* {FakeData.carList.map(({car, index})=>()} */}
 
       {FakeData.carList.map((car, index) => (
         <CarItem car={car} key={index} />
